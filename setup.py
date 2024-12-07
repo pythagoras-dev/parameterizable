d1700b97870081d2a9cd3def96fd36c2ded8fedd
Vlad Pavlov <vlpavlov@ieee.org>
2024-12-07 13:00:23 -0800

    BLD: elevate version to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="parameterizable"
-    ,version="0.2.0"
+    ,version="0.2.1"
     ,author="Vlad (Volodymyr) Pavlov"
     ,author_email="vlpavlov@ieee.org"
     ,description= "Library for work with parameterizable classes."
