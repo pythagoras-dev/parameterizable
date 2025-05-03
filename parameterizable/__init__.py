9991a824f02bf4e689da8248b1ad5c17eb4f82ae
Vlad Pavlov <vlpavlov@ieee.org>
2025-05-03 18:29:21 -0500

    CNL: polish __init__.py

--- parameterizable/__init__.py
+++ parameterizable/__init__.py
@@ -1,1 +1,30 @@
-from .parameterizable import *
+"""Package for working with parameterizable classes.
+
+This package provides functionality for working with parameterizable classes:
+classes that have (hyper) parameters which define object's configuration,
+but not its internal contents or data. Such parameters are typically
+passed to the .__init__() method.
+
+The package provides an API for getting parameters' values from an object,
+and for converting the parameters to and from a portable dictionary
+(a dictionary that only contains basic types and portable sub-dictionaries).
+"""
+
+from .parameterizable import (
+    # Classes
+    ParameterizableClass,
+
+    # Functions
+    get_object_from_portable_params,
+    is_parameterizable,
+    register_parameterizable_class,
+    is_registered
+)
+
+__all__ = [
+    'ParameterizableClass',
+    'get_object_from_portable_params',
+    'is_parameterizable',
+    'register_parameterizable_class',
+    'is_registered'
+]
