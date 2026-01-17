46660e5dbd4e2e31b587f69a8096a0d796cd0a5d
Vlad Pavlov <vlpavlov@ieee.org>
2026-01-17 12:20:34 -0600

    DOC: Add module-level docstring for utility_functions

--- src/mixinforge/utility_functions/__init__.py
+++ src/mixinforge/utility_functions/__init__.py
@@ -1,3 +1,14 @@
+"""Utilities for mixinforge.
+
+This package provides a collection of utility functions used throughout the
+library, including:
+- JSON serialization for complex objects
+- Atomic type detection
+- Nested collection inspection and transformation
+- Dictionary sorting
+- Notebook environment detection
+"""
+
 from .atomics_detector import *
 from .dict_sorter import *
 from .json_processor import *
